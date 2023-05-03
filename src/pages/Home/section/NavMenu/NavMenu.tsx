8ee9543fbf0f28678583c73911f54620b133d96b
Samuel Kim <sams404link@gmail.com>
2023-05-03 16:57:14 -0700

    stabilzed the nav menu

--- src/pages/Home/section/NavMenu/NavMenu.tsx
+++ src/pages/Home/section/NavMenu/NavMenu.tsx
@@ -4,33 +4,49 @@ import "./NavMenu.scss"
 
 export default function NavMenu() {
   const dispatch = useDispatch();
+
+  /*
+  useEffect(() => {
+    document.getElementById("nav-menu")?.addEventListener("click", (event: Event) => {
+      console.log((event.currentTarget as HTMLElement).id);
+    })
+  }, [])
+  */
   return (
-    <section id="nav-menu" className="nav-menu menu-init-state" onClick={() => { dispatch(toggleMenuState()); }}>
+    <section id="nav-menu" className="nav-menu menu-init-state" /*onClick={() => { dispatch(toggleMenuState()); }} */>
       <div id="nav-button-group" className="menu-display-none">
         <h1>Links</h1>
-        <div>
+        <div onClick={() => { dispatch(toggleMenuState()); }}>
           <a className="btn-nav-link" href="#">Home &rarr;</a>
         </div>
-        <div>
+        <div onClick={() => { dispatch(toggleMenuState()); }}>
           <a className="btn-nav-link" href="#tech-stack">My Tech Stack &rarr;</a>
-        </div>
-        <div>
+        </div >
+        <div onClick={() => { dispatch(toggleMenuState()); }}>
           <a className="btn-nav-link" href="#projects">My Projects &rarr;</a>
         </div>
-        <div>
+        <div onClick={() => { dispatch(toggleMenuState()); }}>
           <a className="btn-nav-link" href="#contact">Contact Info &rarr;</a>
         </div>
-        <div>
+        <div
+          id="github-profile"
+        >
           <a className="btn-nav-link" href="https://github.com/IllSmithDa" target="_blank">Github Profile &rarr;</a>
         </div>
-        <div>
+        <div
+          id="linkedin-profile"
+        >
           <a className="btn-nav-link" href="https://www.linkedin.com/in/samuel-kim-1336ab112/" target="_blank">LinkedIn Profile &rarr;</a>
         </div>
-        <div>
-          <a className="btn-nav-link" href="https://www.theologianspen.com" target="_blank">Theologians's Pen &rarr;</a>
+        <div
+          id="theologians-app"
+        >
+          <a className="btn-nav-link" href="https://www.theologianspen.com" target="_blank">Theologian's Pen &rarr;</a>
         </div>
-        <div>
-          <a className="btn-nav-link" href="https://www.gokloak.com" target="_blank">Gokloak &rarr;</a>
+        <div
+          id="kloak-app"
+        >
+          <a className="btn-nav-link" href="https://www.gokloak.com" target="_blank">Kloak &rarr;</a>
         </div>
       </div>
     </section>
