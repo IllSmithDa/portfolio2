c7744f9fecadbc9c66136420df068188953e06d4
Samuel Kim <sams404link@gmail.com>
2023-05-03 18:40:13 -0700

    header edit

--- src/pages/Home/section/Header/Header.tsx
+++ src/pages/Home/section/Header/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <header className="header">
       <h1>Samuel Kim</h1>
-      <h3>Full Stack Developer from Bay Area Ca</h3>
+      <h3>Full Stack Developer From SF Ca</h3>
       <a href="#tech-stack" className="left-on-scroll btn btn--white">Learn More</a>
     </header>
   );
