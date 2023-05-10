28816ee1285559e3e693e78f9b92d21bbb456a6c
Samuel Kim <sams404link@gmail.com>
2023-05-09 23:49:02 -0700

    removed imgur references

--- src/pages/Home/section/Contact/Contact.tsx
+++ src/pages/Home/section/Contact/Contact.tsx
@@ -1,8 +1,11 @@
 import "./Contact.scss";
+import myPicture from '../../../../assets/images/SamK.png'
+import githubImg from '../../../../assets/images/github.png'
+import linkedinImg from '../../../../assets/images/linkedin.png'
 export default function Contact() {
   return (
     <section className="contact-container" id="contact">
-      <img className="my-image right-on-scroll" alt='profile' src="https://i.imgur.com/DwUV9bu.jpg" />
+      <img className="my-image right-on-scroll" alt='profile' src={myPicture} />
       <h1 className="heading-secondary u-center-text u-margin-bottom-small right-on-scroll">Contact</h1>
       <p className="u-margin-bottom-small left-on-scroll"><b>Best way to Contact: </b>Email or Phone</p>
       <p className="u-margin-bottom-small left-on-scroll"><b>Email: </b>sam404kim@gmail.com</p>
@@ -10,10 +13,10 @@ export default function Contact() {
       <p className="u-margin-bottom-small left-on-scroll"><b>Other ways to connect:</b></p>
       <div className="icons left-on-scroll">
         <a href="https://github.com/IllSmithDa" target="_blank" rel="noopener noreferrer">
-          <img alt="linkedin_link" src="https://i.imgur.com/dQqam06.png" className="image-icon" />
+          <img alt="linkedin_link" src={githubImg} className="image-icon" />
         </a>
         <a href="https://www.linkedin.com/in/samuel-kim-1336ab112/" target="_blank" rel="noopener noreferrer">
-          <img alt="linkedin_link" src="https://i.imgur.com/AfBFS6f.png" className="image-icon" />
+          <img alt="linkedin_link" src={linkedinImg} className="image-icon" />
         </a>
       </div>
     </section>
