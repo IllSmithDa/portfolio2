6e631c70844349eb58333f1746ac1ab1d0134332
Samuel <sam404kim@gmail.com>
2023-10-19 13:40:59 -0700

    attempt at improving img load speed

--- src/pages/Home/section/Header/Header.tsx
+++ src/pages/Home/section/Header/Header.tsx
@@ -2,9 +2,19 @@ import "./Header.scss";
 export default function Header() {
   return (
     <header className="header">
+      {
+      <img
+        className='bg-img'
+        src={('/src/assets/images/forest_bridge_com.webp')}
+        loading='lazy'
+      />
+  }
       <h1>Samuel Kim</h1>
       <h3 className="u-margin-bottom-large">Full Stack Developer From SF Ca</h3>
       <a href="#tech-stack" className="left-on-scroll btn btn--white">Learn More</a>
+      {
+        <div className='bg-gradient'></div>
+      }
     </header>
   );
 }
