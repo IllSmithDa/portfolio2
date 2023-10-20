0bd22e78746c353c206c05421a35067d708e6c97
Samuel <sam404kim@gmail.com>
2023-10-19 17:06:44 -0700

    fixed height issues

--- src/pages/Home/section/Project/Project.tsx
+++ src/pages/Home/section/Project/Project.tsx
@@ -83,9 +83,13 @@ export default function Project() {
         loading='lazy'
       />
       <h1 className="u-margin-bottom-large right-on-scroll">My Projects and Demos</h1>
-      <div className="project-display left-on-scroll">
-        {listProjects}
-      </div>
+      <section className='arrow-container'>
+        <button className="arrow-btn">&#11164;</button>
+        <div className="project-display left-on-scroll">
+          {listProjects}
+        </div>
+        <button className="arrow-btn">&#11166;</button>
+      </section>
       <div className='bg-gradient'></div>
     </section>
   );
